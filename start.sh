056cad380c68b615cf1f46d1a27a4bc068f8a015
EWANZO101 <107235474+EWANZO101@users.noreply.github.com>
2024-07-09 19:31:42 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,86 @@
+sudo apt update && aptupgrade
+
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+
+sudo apt install nodejs
+
+npm install -g pnpm
+
+
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+
+
+sudo apt update && sudo apt install postgresql-14 postgresql-contrib
+
+
+sudo systemctl start postgresql.service
+
+
+sudo -u postgres -i
+
+psql -d postgres
+
+CREATE USER "snailycad";
+
+
+ALTER USER "snailycad" WITH SUPERUSER;
+
+
+ALTER USER "snailycad" PASSWORD 'zVw&HJBf8W8tmBu';
+
+
+CREATE DATABASE "snaily-cadv4";
+
+
+exit 
+
+exit
+
+sudo -s 
+
+
+##################################################
+
+cd /home/
+
+
+git clone https://github.com/SnailyCAD/snaily-cadv4.git
+
+
+cd snaily-cadv4
+
+
+
+pnpm install
+
+
+### wait untill its done ##
+
+
+
+cp .env.example .env
+
+
+
+nano .env
+
+when done ### ctrl X yes  enter ctrl C
+
+## Then make the code ## 
+
+####################################
 nano /home/snaily-cadv4/start.sh
+
+
+
+#####################
+
+copy code below then right click in the
+
+#####
+
+
+
 
 #!/bin/bash
 
@@ -23,12 +105,16 @@ pnpm run build
 pnpm run start
 echo "All processes are completed."
 
+####################  END  #############
 
 
-
-
+###
 
  nano /etc/systemd/system/start-snaily-cadv4.service        
+
+
+#####
+
 
 [Unit]
 Description=Start Snaily CADv4
@@ -46,12 +132,18 @@ WorkingDirectory=/home/snaily-cadv4
 WantedBy=multi-user.target
 
 
+###
+
 chmod +x /home/snaily-cadv4/start.sh
 
+
+####
+
+#####
 sudo systemctl daemon-reload
 sudo systemctl enable start-snaily-cadv4.service
  sudo systemctl start start-snaily-cadv4.service
   sudo systemctl restart start-snaily-cadv4.service
-
+#####
 
    cat /home/snaily-cadv4/start.log
